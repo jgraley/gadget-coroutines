84a000d4cbdce042b9010eefeae16b5bf1f8ea56
jgraley <jgraley1@gmail.com>
2020-03-01 18:52:05 +0000

    minor fixes and use static cast

--- arm-specifics.h
+++ arm-specifics.h
@@ -7,12 +7,11 @@ namespace gco
 static const int ARM_JMPBUF_INDEX_SP = 8;
 inline void *get_sp( jmp_buf env )
 {
-    return (void *)( env[ARM_JMPBUF_INDEX_SP] );
+    return static_cast<void *>( env[ARM_JMPBUF_INDEX_SP] );
 }
-
 inline void set_sp( jmp_buf env, void *new_sp )
 {
-    env[ARM_JMPBUF_INDEX_SP] = new_sp;
+    env[ARM_JMPBUF_INDEX_SP] = static_cast<int>(new_sp);
 }
-
+#endif
 };
