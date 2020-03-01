4779e0da9e6264f78e1d45afee510fb09a26ad6f
jgraley <jgraley1@gmail.com>
2020-03-01 18:45:51 +0000

    Create arm-specifics.h
    Starting with some HAL-like stuff

---
+++ arm-specifics.h
@@ -0,0 +1,18 @@
+#include <csetjmp>
+
+namespace gco
+{
+
+#if defined(__arm__) || defined(__thumb__)
+static const int ARM_JMPBUF_INDEX_SP = 8;
+inline void *get_sp( jmp_buf env )
+{
+    return (void *)( env[ARM_JMPBUF_INDEX_SP] );
+}
+
+inline void set_sp( jmp_buf env, void *new_sp )
+{
+    env[ARM_JMPBUF_INDEX_SP] = new_sp;
+}
+
+};
