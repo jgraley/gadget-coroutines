7c9fc3971b55be2e988b7ad2fc1cb9acb8eff697
John Graley <jgraley1@gmail.com>
2020-04-04 21:05:09 +0100

    more progress on super functors

--- src/SuperFunctor.cpp
+++ src/SuperFunctor.cpp
@@ -3,30 +3,62 @@
 #include "SuperFunctor.h"
 #include "CoroTracing.h"
 
-SuperFunctor::SuperFunctor()
+using namespace std;
+
+SuperFunctor::SuperFunctor() :
+   entrypoint_mfp( &SuperFunctor::operator() )
+{
+    const pair<ThumbInstruction *, int> assembly = GetAssembly();
+    ASSERT( assembly.second <= MAX_INSTRUCTIONS, "Thunk assmebly too long" );
+    memcpy( entrypoint_thunk, assembly.first, assembly.second * sizeof(ThumbInstruction) );     
+}
+
+
+void SuperFunctor::SetEntrypoint( EntryPoint ep )
+{
+    // TODO deconstruct ep into a normal funciton pointer using "this",
+    // since we declare the virtual operator()
+    // See if we can just reference operator() directly (we're not in the 
+    // constructor, so it might work) 
+    entrypoint_mfp = ep;
+
+    // TODO and then, for efficiency, modify the thunk code with a direct branch
+}
+
+
+void TestClient::operator()()
 {
 }
 
 
-void SuperFunctor::operator()()
+pair<SuperFunctor::ThumbInstruction *, int> SuperFunctor::GetAssembly()
 {
+    volatile bool actually_run_it = false;
+    if( actually_run_it )
+    {
+        BEGIN:
+        asm( "mov r0, pc\n\t" // Get PC into "this" pointer
+             "sub r0, %[this_PC_offset]\n\t" // Correct for pipeline effect in PC
+             "ldr r1, [r0, %[this_entry_offset]]\n\t" // Get function pointer after this code
+             "bx  r1" // Jump to it (tail call)
+              : : [this_PC_offset] "I" (offsetof(SuperFunctor, entrypoint_thunk)+4),
+                  [this_entry_offset] "M" (offsetof(SuperFunctor, entrypoint_mfp)) : );            
+        END:
+            for(;;);
+    }
+    
+    ThumbInstruction *begin = (ThumbInstruction *)(&&BEGIN);
+    ThumbInstruction *end = (ThumbInstruction *)(&&END);
+    
+    return make_pair( begin, end-begin );
 }
 
-
-void SuperFunctor::ReferenceAssmebly()
-{
-    asm( "mov r0, pc\n\t"
-         "ldr r1, [r0, #8]\n\t"
-         "bx  r1"
-          : : : );            
-}
-
-
-void SuperFunctor::TestPCRelative()
+#ifdef SUPERFUNCTOR_TESTS  
+void TestClient::TestPCRelative()
 {
     uint8_t *pc_grab;
     asm( "mov %[result], pc" :  [result] "=r" (pc_grab)  : : );    
-    TRACE("Grabbed PC: %p; function pointer:%p", pc_grab, &SuperFunctor::TestPCRelative);
+    TRACE("Grabbed PC: %p; function pointer:%p", pc_grab, &TestClient::TestPCRelative);
     /* What I learned:
      * - Grabbed PC is ahead of the mov instruction by 4 bytes (actually 2 
      *   instructions, I think, so only correct in thumb mode)
@@ -36,19 +68,26 @@ void SuperFunctor::TestPCRelative()
 }
 
 
-void SuperFunctor::TestMemberFunctionPointer()
+void TestClient::TestMemberFunctionPointer()
 {
-    void (SuperFunctor::* volatile mfp)() = &SuperFunctor::operator();
+    void (TestClient::* volatile mfp)() = &TestClient::operator();
     TRACE("Size of member function pointer: %d", sizeof(mfp));
     TRACE("As uint32_t: %x %x", ((uint32_t *)&mfp)[0], ((uint32_t *)&mfp)[1] );
-    SuperFunctor s;
+    TestClient s;
     (s.*mfp)();
+    TRACE("Thises: %p %p", &s, (SuperFunctor *)&s );
+    TRACE("entrypoint_thunk: %p", &(s.entrypoint_thunk) );
+
     /* What I learned:
-     * - Size of MFP is 8 bytes, made up of regular function pointer 
-     *   followed by 0. From the assembly, I suspect undefined. Again,
-     *   bit 0 of the FP has been set to 1.
-     * - MFP invoked using blx instruction directly on the FP part of 
-     *   the MFP
-     */
-    
+     * - Size of MFP is 8 bytes: Updated:
+     * If non-virtual: { FP, 0 }
+     * If virtual: { vtable-offset, (object-offset<<1) | 1 }
+     * 
+     * To invoke function given { P, Q }:
+     * if( Q.0 == 0 )
+     *   call P
+     * else
+     *   call *( *(object+(Q>>1)) + P )
+     */   
 }
+#endif
--- src/SuperFunctor.h
+++ src/SuperFunctor.h
@@ -1,16 +1,35 @@
 #ifndef SUPERFUNCTOR_H
 #define SUPERFUNCTOR_H
 
+#include <utility>
+#include <cstddef>
+#include <cstdint>
+
+#define MAX_INSTRUCTIONS 4
+#define SUPERFUNCTOR_TESTS
+
 class SuperFunctor
 {
 public:
+  typedef void (SuperFunctor::* EntryPoint)();
   SuperFunctor();
-  void operator()();
-  static void TestPCRelative();
-  static void TestMemberFunctionPointer();
-  
-private:    
-  static void ReferenceAssmebly();
+  void SetEntrypoint( EntryPoint ep );
+  virtual void operator()();    
+
+public:     // TODO private
+  typedef uint16_t ThumbInstruction;
+  static std::pair<ThumbInstruction *, int> GetAssembly();
+  ThumbInstruction entrypoint_thunk[MAX_INSTRUCTIONS];
+  EntryPoint entrypoint_mfp; // TODO be a normal function pointer
 };
+
+#ifdef SUPERFUNCTOR_TESTS  
+class TestClient : SuperFunctor
+{
+public:
+  static void TestPCRelative();
+  static void TestMemberFunctionPointer(); 
+};
+#endif
 
 #endif
